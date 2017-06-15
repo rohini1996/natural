7fb4e3a5681c3870ab5caab28feba2ece3a9e701
rohini1996 <rohini111222@gmail.com>
2017-06-15 18:04:50 +0530

    natural.java

---
+++ natural.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+class natural
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int number=s.nextInt();
+int i=1,sum=0;
+while(i<=number)
+{
+sum=sum+i;
+i++;
+}
+System.out.println(sum);
+})
